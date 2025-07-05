8117ad735d35c2ff7a5ffce96f0a58ee59fe1db2
JTrumble21 <jessi.oostrom@triosstudent.com>
2025-07-05 01:54:36 -0400

    debug

--- AngularApp2/src/app/app.config.ts
+++ AngularApp2/src/app/app.config.ts
@@ -1,13 +1,37 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { HttpClientModule } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { serverRoutes } from './app.routes.server';
 
-export const appConfig: ApplicationConfig = {
+// Base app config for both client and server
+const baseAppConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    importProvidersFrom(HttpClientModule)
+  ]
+};
+
+// Server-specific config
+const serverConfig: ApplicationConfig = {
+  providers: [
+    provideServerRendering(withRoutes(serverRoutes))
+  ]
+};
+
+// Export merged config for SSR usage
+export const appConfig: ApplicationConfig = {
+  ...baseAppConfig,
+  providers: [
+    ...baseAppConfig.providers!,
+    ...serverConfig.providers!
   ]
 };
